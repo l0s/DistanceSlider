36bf3dc9bd6b4a49f8daf432b1eb90284eae8759
Carlos Macasaet <github@carlos-m.net>
2015-06-29 10:07:59 -0700

    logarithmic slider implementation

--- DistanceSlider/ViewController.swift
+++ DistanceSlider/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var slider: UISlider!
+
+    let base = Double( 200 )
+    let min = Double( 10 )
+    let max = Double( 3500 )
+
+    var distanceInMiles = 50
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        slider.value = 0.1855 // sets starting radius to ~50 (will vary depending on the base)
+        updateDistance( slider, forEvent: UIEvent() )
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func calculateDistance( sliderValue:Float ) -> Int
+    {
+        // source: http://stackoverflow.com/questions/19277653/exponential-algorithm-for-a-slider-control
+        let doubleValue = ( max - min ) * ( pow( base, Double( sliderValue ) ) - pow( base, -Double( sliderValue ) ) ) / ( pow( base, Double( 1.0 ) ) - pow( base, Double( -1.0 ) ) ) + min
+        let roundedValue = round( doubleValue / 5.0 ) * 5.0
+        return Int( roundedValue )
     }
 
+    @IBAction func updateDistance(sender: UISlider, forEvent event: UIEvent) {
+        distanceInMiles = calculateDistance( sender.value )
+
+        label.text = "\(distanceInMiles) Miles"
+    }
 
 }
-
